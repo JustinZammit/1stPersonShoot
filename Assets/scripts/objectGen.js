55ba3a590cf2339db7f9a011aa6ea4e60bd6ca7a
MiguelMizzi <miguelmizzi94@gmail.com>
2014-04-22 12:31:10 +0200

    FolderStructure+BasicFunctionality
    I made the folder structure so that I can put all the assets in their
    respective place. Also I started working on functionality and I have
    prefabs ready but the ray which I will use to destroy enemies with is
    currently not working

---
+++ Assets/scripts/objectGen.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var cubeObject:Rigidbody;
+var sphereObject:Rigidbody;
+
+
+function generateCubes()
+{
+	//infinite loop (on purpose)
+	while(true)
+	{
+		//add the code to instantiate cubes.  This is creating one cube per second
+		//exercise 1: Add generation of spheres
+		Instantiate(cubeObject,Vector3(Random.Range(-6,6)+transform.position.x,Random.Range(-5,5)+transform.position.y,transform.position.z),transform.rotation);
+		yield WaitForSeconds(1.0);
+	}
+}
+
+
+function Start () {
+	//generate one object per second
+	yield StartCoroutine("generateCubes");
+}
+
+function Update () {
+	
+}
---
+++ Assets/scripts/objectMovement.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var objectSpeed:int;
+
+
+function Start () {
+	//set a random speed
+	objectSpeed = Random.Range(5,25);
+}
+
+function Update () {
+	//move the objects towards the camera
+	transform.Translate(Vector3.back * objectSpeed * Time.deltaTime);
+}
+
+
+function OnBecameInvisible()
+{
+	//Exercise 3: Implement missed cubes counter
+	Destroy(this.gameObject);
+}
